62127602271391a487868c861ea38d815763d157
Lucas-Cours <tp98093@gmail.com>
2025-10-01 17:43:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -14,13 +14,19 @@ searchBtn.addEventListener("click", async () => {
 
     if (data.main.temp < 10) {
     document.body.style.backgroundColor = "rgba(212, 212, 250, 1)";
-    console.log("cold");
+    console.log("cold");    
+    document.body.style.backgroundImage = "url(https://imgs.search.brave.com/3se0BxcK9ZNW8JBd47UeAeKPcSd6nKEEOovgZrB8IzM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tYXJr/ZXRwbGFjZS5jYW52/YS5jb20vTUFEcEVo/QUxET1UvMS90aHVt/Ym5haWxfbGFyZ2Uv/Y2FudmEtd2ludGVy/LWJhY2tncm91bmQt/TUFEcEVoQUxET1Uu/anBn)";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
 }
  else if (data.main.temp >= 10 && data.main.temp <= 20) {
     document.body.style.backgroundColor = "#2fb37cab";
 } else {
     document.body.style.backgroundColor = "#f00";
     console.log("hot");
+    document.body.style.backgroundImage="url(https://imgs.search.brave.com/jiBtnMrZ1EdjdQ3eXo6xhvdcTcUoF5GpFemP8cMIIcQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS12ZWN0b3Iv/cmVhbGlzdGljLWhv/dC1iYWNrZ3JvdW5k/XzIzLTIxNDk0NTYw/MzMuanBnP3NpemU9/NjI2JmV4dD1qcGc)"
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
 }
 
     if (data.cod === "404") {
